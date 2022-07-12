10c74332561ca1b0d741c0e7b8c6d93accb8422a
SurajOP01 <93099790+SurajOP01@users.noreply.github.com>
2022-07-12 19:05:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,15 +42,6 @@ if sudo:
 
 aiohttpsession = aiohttp.ClientSession()
 
-#-------------------------BOT-----------------------------
-
-bot = Client(
-    'AltronUserbot',
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins={'root': 'AltronUserbot.bot'},
-)
 
 #-------------------------CLIENTS-----------------------------
 
