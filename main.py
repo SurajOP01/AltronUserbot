938836d415d05384b45b260483f382dea22928ba
SurajOP01 <93099790+SurajOP01@users.noreply.github.com>
2022-07-12 18:53:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 from pyrogram import idle
-from config import client, client2, client3, client4, client5, client6, client7, client8, client9, client10, bot, call_py, call_py2, call_py3, call_py4, call_py5, call_py6, call_py7, call_py8, call_py9, call_py10
+from config import client, client2, client3, client4, client5, client6, client7, client8, client9, client10, call_py, call_py2, call_py3, call_py4, call_py5, call_py6, call_py7, call_py8, call_py9, call_py10
 
 async def main():
     if client:
@@ -112,7 +112,6 @@ async def main():
             await client10.join_chat("HeroOfficialBots")
         except Exception as e:
             print(str(e))
-    await bot.start()
     if call_py:
         await call_py.start()
     if call_py2:
