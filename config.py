28329e03e28fa472c63a288675b6b7fd9474a6a9
SurajOP01 <93099790+SurajOP01@users.noreply.github.com>
2022-07-13 10:26:50 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -8,25 +8,20 @@ from pyrogram import Client
 from pyrogram.types import *
 from pytgcalls import PyTgCalls
 
-logging.basicConfig(level=logging.INFO)
 
+logging.basicConfig(level=logging.INFO)
                     
 if os.path.exists("Internal"):
     load_dotenv("Internal")
 
-API_ID = int(getenv("API_ID", "17492714"))
-API_HASH = getenv("API_HASH", "26b7fd64901c7d3c6276cd06e272ce95")
+
+#---------------------DON'T MESS WITH THESE REQUIRED CODES-------------------------------
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_TOKEN = getenv("BOT_TOKEN")
+SESSION = getenv("SESSION")
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/TheAltronX/AltronUserbot")
-SESSION = getenv("SESSION", "BQCPMtfPCvH_QDY61g8-9XPKoiL_pCXmDRxloHKh1QTHNbrLyiql7KgZJ5MIw9gVSOYcK14o1ZGpdk9Hwaui5SH-mzVzhkGyqrapq7MYau9h7loy7SaRS38qISV3yzF3qNcVP-4ouGVOt0btkOG__BRsTw30ovp--NLabVDlbAO_LCnzAcq3kpmBxK_HHko1aG_Bj8BAESr4CAHTP0gO5VWak-LFcl43Q4LLRmwWTPe6AB1H9UdVdQPGvVnhlGYHPHi-SrP-n4LETM0lwi9Uz6YFbMb94s2xOwEyoy5VmbEvbRSEyIol4f604TjF5cwR7pzz7zLGX5TBqI0-FmfzPuY2AAAAATTHWc4A")
-SESSION2 = getenv("SESSION2", "")
-SESSION3 = getenv("SESSION3", "")
-SESSION4 = getenv("SESSION4", "")
-SESSION5 = getenv("SESSION5", "")
-SESSION6 = getenv("SESSION6", "")
-SESSION7 = getenv("SESSION7", "")
-SESSION8 = getenv("SESSION8", "")
-SESSION9 = getenv("SESSION9", "")
-SESSION10 = getenv("SESSION10", "")
 
 def make_int(str_input):
     str_list = str_input.split(" ")
@@ -35,81 +30,102 @@ def make_int(str_input):
         int_list.append(int(x))
     return int_list
   
-sudo = getenv("SUDO_USERS", "1323020756")
+sudo = getenv("SUDO_USERS")
 SUDO_USERS = []
 if sudo:
     SUDO_USERS = make_int(sudo)
 
 aiohttpsession = aiohttp.ClientSession()
 
+#-----------------------------OPTIONAL--------------------------------
+
+SESSION2 = getenv("SESSION2")
+SESSION3 = getenv("SESSION3")
+SESSION4 = getenv("SESSION4")
+SESSION5 = getenv("SESSION5")
+SESSION6 = getenv("SESSION6")
+SESSION7 = getenv("SESSION7")
+SESSION8 = getenv("SESSION8")
+SESSION9 = getenv("SESSION9")
+SESSION10 = getenv("SESSION10")
+
+#-------------------------BOT-----------------------------
+
+bot = Client(
+    'AltronUserbot',
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins={'root': 'plugins.bot'},
+)
 
 #-------------------------CLIENTS-----------------------------
 
 if SESSION:
-    client = Client(SESSION, API_ID, API_HASH, plugins=dict(root="AltronUserbot"))
+    client = Client(SESSION, API_ID, API_HASH, plugins=dict(root="plugins"))
     call_py = PyTgCalls(client, overload_quiet_mode=False)
 else:
     client = None
     call_py = None
 
 if SESSION2:
-    client2 = Client(SESSION2, API_ID, API_HASH, plugins=dict(root="AltronUserbot"))
+    client2 = Client(SESSION2, API_ID, API_HASH, plugins=dict(root="plugins"))
     call_py2 = PyTgCalls(client2, overload_quiet_mode=False)
 else:
     client2 = None
     call_py2 = None
 
 if SESSION3:
-    client3 = Client(SESSION3, API_ID, API_HASH, plugins=dict(root="AltronUserbot"))
+    client3 = Client(SESSION3, API_ID, API_HASH, plugins=dict(root="plugins"))
     call_py3 = PyTgCalls(client3, overload_quiet_mode=False)
 else:
     client3 = None
     call_py3 = None
 
 if SESSION4:
-    client4 = Client(SESSION4, API_ID, API_HASH, plugins=dict(root="AltronUserbot"))
+    client4 = Client(SESSION4, API_ID, API_HASH, plugins=dict(root="plugins"))
     call_py4 = PyTgCalls(client4, overload_quiet_mode=False)
 else:
     client4 = None
     call_py4 = None
 
 if SESSION5:
-    client5 = Client(SESSION5, API_ID, API_HASH, plugins=dict(root="AltronUserbot"))
+    client5 = Client(SESSION5, API_ID, API_HASH, plugins=dict(root="plugins"))
     call_py5 = PyTgCalls(client5, overload_quiet_mode=False)
 else:
     client5 = None
     call_py5 = None
 
 if SESSION6:
-    client6 = Client(SESSION6, API_ID, API_HASH, plugins=dict(root="AltronUserbot"))
+    client6 = Client(SESSION6, API_ID, API_HASH, plugins=dict(root="plugins"))
     call_py6 = PyTgCalls(client6, overload_quiet_mode=False)
 else:
     client6 = None
     call_py6 = None
         
 if SESSION7:
-    client7 = Client(SESSION7, API_ID, API_HASH, plugins=dict(root="AltronUserbot"))
+    client7 = Client(SESSION7, API_ID, API_HASH, plugins=dict(root="plugins"))
     call_py7 = PyTgCalls(client7, overload_quiet_mode=False)
 else:
     client7 = None
     call_py7 = None
 
 if SESSION8:
-    client8 = Client(SESSION8, API_ID, API_HASH, plugins=dict(root="AltronUserbot"))
+    client8 = Client(SESSION8, API_ID, API_HASH, plugins=dict(root="plugins"))
     call_py8 = PyTgCalls(client8, overload_quiet_mode=False)
 else:
     client8 = None
     call_py8 = None
 
 if SESSION9:
-    client9 = Client(SESSION9, API_ID, API_HASH, plugins=dict(root="AltronUserbot"))
+    client9 = Client(SESSION9, API_ID, API_HASH, plugins=dict(root="plugins"))
     call_py9 = PyTgCalls(client9, overload_quiet_mode=False)
 else:
     client9 = None
     call_py9 = None
 
 if SESSION10:
-    client10 = Client(SESSION10, API_ID, API_HASH, plugins=dict(root="AltronUserbot"))
+    client10 = Client(SESSION10, API_ID, API_HASH, plugins=dict(root="plugins"))
     call_py10 = PyTgCalls(client10, overload_quiet_mode=False)
 else:
     client10 = None
